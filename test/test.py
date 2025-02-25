3787afe1ba423cf738da580da8b761f5fbd9c4df
WilliamW <billywei0925@gmail.com>
2025-02-25 02:24:46 -0500

    Update test.py

--- test/test.py
+++ test/test.py
@@ -18,7 +18,7 @@ async def test_project(dut):
     dut._log.info("Reset")
     dut.ena.value = 1
     dut.ui_in.value = 0
-    #dut.uio_in.value = 0
+    dut.uio_in.value = 0
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1

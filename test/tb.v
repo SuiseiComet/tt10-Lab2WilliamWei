4178b303bd0b0324d4d74aa729a7c81889cdb795
WilliamW <billywei0925@gmail.com>
2025-02-24 21:40:52 -0500

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -27,7 +27,7 @@ module tb ();
   wire VGND = 1'b0;
 `endif
 
-  // Replace tt_um_example with your module name:
+  // Replace tt_um_Lab2WilliamWei with your module name:
   tt_um_example user_project (
 
       // Include power ports for the Gate Level test:

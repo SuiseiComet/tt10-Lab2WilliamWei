dfb37aa3deeceab2e792d111d3b4b0840bce4b2d
WilliamW <billywei0925@gmail.com>
2025-02-24 23:15:59 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -18,23 +18,23 @@ module tt_um_project (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-if (In[15] == 1) uo_out = 8'b0000_1111;
-    else if (In[14] == 1) assign uo_out = 8'b0000_1110;
-    else if (In[13] == 1) assign uo_out = 8'b0000_1101;
-    else if (In[12] == 1) assign uo_out = 8'b0000_1100;
-    else if (In[11] == 1) assign uo_out = 8'b0000_1011;
-    else if (In[10] == 1) assign uo_out = 8'b0000_1010;
-    else if (In[9]  == 1) assign uo_out = 8'b0000_1001;
-    else if (In[8]  == 1) assign uo_out = 8'b0000_1000;
-    else if (In[7]  == 1) assign uo_out = 8'b0000_0111;
-    else if (In[6]  == 1) assign uo_out = 8'b0000_0110;
-    else if (In[5]  == 1) assign uo_out = 8'b0000_0101;
-    else if (In[4]  == 1) assign uo_out = 8'b0000_0100;
-    else if (In[3]  == 1) assign uo_out = 8'b0000_0011;
-    else if (In[2]  == 1) assign uo_out = 8'b0000_0010;
-    else if (In[1]  == 1) assign uo_out = 8'b0000_0001;
-    else if (In[0]  == 1) assign uo_out = 8'b0000_0000;
+// All output pins must be assigned. If not used, assign to 0.
+if (ui_in[15] == 1) uo_out = 8'b0000_1111;
+    else if (ui_in[14] == 1) assign uo_out = 8'b0000_1110;
+    else if (ui_in[13] == 1) assign uo_out = 8'b0000_1101;
+    else if (ui_in[12] == 1) assign uo_out = 8'b0000_1100;
+    else if (ui_in[11] == 1) assign uo_out = 8'b0000_1011;
+    else if (ui_in[10] == 1) assign uo_out = 8'b0000_1010;
+    else if (ui_in[9]  == 1) assign uo_out = 8'b0000_1001;
+    else if (ui_in[8]  == 1) assign uo_out = 8'b0000_1000;
+    else if (ui_in[7]  == 1) assign uo_out = 8'b0000_0111;
+    else if (ui_in[6]  == 1) assign uo_out = 8'b0000_0110;
+    else if (ui_in[5]  == 1) assign uo_out = 8'b0000_0101;
+    else if (ui_in[4]  == 1) assign uo_out = 8'b0000_0100;
+    else if (ui_in[3]  == 1) assign uo_out = 8'b0000_0011;
+    else if (ui_in[2]  == 1) assign uo_out = 8'b0000_0010;
+    else if (ui_in[1]  == 1) assign uo_out = 8'b0000_0001;
+    else if (ui_in[0]  == 1) assign uo_out = 8'b0000_0000;
     else assign uo_out = 8'b1111_0000; // Special case: all zeros
 
   //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in

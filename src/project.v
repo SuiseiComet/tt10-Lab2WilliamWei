34473bb45745d33d358c207e2363a6d3ed454f67
WilliamW <billywei0925@gmail.com>
2025-02-24 23:39:57 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -18,9 +18,10 @@ module tt_um_project (
     input  wire       rst_n     // reset_n - low to reset
 );
 
+// All output pins must be assigned. If not used, assign to 0.
+if (ui_in[15] == 1) uo_out = 8'b0000_1111;
 
 
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
   assign uio_in = 0;

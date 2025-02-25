481bbff71a964ae2e09a1158347fc78ae3b392b2
WilliamW <billywei0925@gmail.com>
2025-02-24 23:25:46 -0500

    Update project.v

--- src/project.v
+++ src/project.v
@@ -18,26 +18,9 @@ module tt_um_project (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-// All output pins must be assigned. If not used, assign to 0.
-if (ui_in[15] == 1) uo_out = 8'b0000_1111;
-    else if (ui_in[14] == 1) assign uo_out = 8'b0000_1110;
-    else if (ui_in[13] == 1) assign uo_out = 8'b0000_1101;
-    else if (ui_in[12] == 1) assign uo_out = 8'b0000_1100;
-    else if (ui_in[11] == 1) assign uo_out = 8'b0000_1011;
-    else if (ui_in[10] == 1) assign uo_out = 8'b0000_1010;
-    else if (ui_in[9]  == 1) assign uo_out = 8'b0000_1001;
-    else if (ui_in[8]  == 1) assign uo_out = 8'b0000_1000;
-    else if (ui_in[7]  == 1) assign uo_out = 8'b0000_0111;
-    else if (ui_in[6]  == 1) assign uo_out = 8'b0000_0110;
-    else if (ui_in[5]  == 1) assign uo_out = 8'b0000_0101;
-    else if (ui_in[4]  == 1) assign uo_out = 8'b0000_0100;
-    else if (ui_in[3]  == 1) assign uo_out = 8'b0000_0011;
-    else if (ui_in[2]  == 1) assign uo_out = 8'b0000_0010;
-    else if (ui_in[1]  == 1) assign uo_out = 8'b0000_0001;
-    else if (ui_in[0]  == 1) assign uo_out = 8'b0000_0000;
-    else assign uo_out = 8'b1111_0000; // Special case: all zeros
 
-  //assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+
+  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
   assign uio_out = 0;
   assign uio_oe  = 0;
   assign uio_in = 0;
